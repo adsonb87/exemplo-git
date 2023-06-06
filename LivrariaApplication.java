ed00ff133386c48b122364006d975473b035a7ac
Adson Souza <adsonsouza@MacBook-Air-de-Adson.local>
2023-06-06 20:15:39 -0300

    Receber idade e verificar maior idade

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -5,8 +5,18 @@ public class LivrariaApplication{
         Scanner scan = new Scanner(System.in);
         System.out.println("nome: ");
         String text = scan.nextLine();
+        System.out.println("Idade: ");
+        Integer idade = scan.nextInt();
         scan.close();
+
         System.out.println("Hello " + text + " !!");
+
+        if(idade >= 18){
+            System.out.println("Você é maior de idade !!!");
+        }else{
+            System.out.println("Você é menor de idade !!!");
+        }
+        
     }
 
 }
