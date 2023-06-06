49c0443aabba224f0d5e5379bbdd265d8c1ad534
Adson Souza <adsonsouza@MacBook-Air-de-Adson.local>
2023-06-06 19:56:31 -0300

    Entrada de dados pelo usuário

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class LivrariaApplication{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("nome: ");
+        String text = scan.nextLine();
+        scan.close();
+        System.out.println("Hello " + text + " !!");
     }
 
 }
